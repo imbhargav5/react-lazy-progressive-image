65a7af0d42e85b483c79fb9a6ddce73771f797c6
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2018-02-19 17:21:41 +0530

    Test production content

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import LazyImage from "../src";
+import LazyImage from "../lib";
 
 configure({ adapter: new Adapter() });
 
